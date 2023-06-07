30725b1899f6ddbb56b7cbc2da4e539367c48389
George Pollard <gpollard@microsoft.com>
2023-06-07 13:24:26 +1200

    Switch to std::io::IsTerminal

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub enum Stream {
 }
 
 fn is_a_tty(stream: Stream) -> bool {
-    use is_terminal::*;
+    use std::io::IsTerminal;
     match stream {
         Stream::Stdout => std::io::stdout().is_terminal(),
         Stream::Stderr => std::io::stderr().is_terminal(),
